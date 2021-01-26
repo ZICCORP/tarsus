8f44f171c2bcb468e7ad9820252ca224a7e06692
ZICCORP <frankchuka250@gmail.com>
2021-01-26 13:04:24 +0100

    Added user registration

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    # Django Admin
     path('admin/', admin.site.urls),
+
+    # User Management
+    path('accounts/',include('django.contrib.auth.urls')),
+    
+
+    # Local apps
     path('',include('pages.urls')),
+    path('accounts/',include('users.urls')),
 ]
