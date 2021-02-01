e115be9afa26c0ae936e2bab095ee07cca3f8096
ZICCORP <frankchuka250@gmail.com>
2021-02-01 12:26:31 +0100

    added user uploaded files configuration

--- store/settings.py
+++ store/settings.py
@@ -124,6 +124,10 @@ STATICFILES_FINDERs = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 AUTH_USER_MODEL='users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT = 'home'
--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,include
 
 urlpatterns = [
@@ -14,4 +16,4 @@ urlpatterns = [
     path('',include('pages.urls')),
     path('products/',include('products.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
