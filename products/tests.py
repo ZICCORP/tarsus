6bfe93dbcfbb83a67aa1a91e18fff57f3272cee6
ZICCORP <frankchuka250@gmail.com>
2021-02-01 17:17:10 +0100

    Added custom permission configurations

--- products/tests.py
+++ products/tests.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 
 from .models import Product,Review
 
+from django.contrib.auth.models import Permission
+
 from django.contrib.auth import get_user_model
 
 
@@ -13,6 +15,7 @@ class ProductTest(TestCase):
         self.user = get_user_model().objects.create_user(username='reviewuser',email='reviewuser@email.com',password='frank123')
         self.product = Product.objects.create(title="Laptop",seller='okenny john',price='29.99')
         self.review = Review.objects.create(seller=self.user,product=self.product,review='absolutely loved it')
+        self.special_permission = Permission.objects.get(codename='special_status')
 
 
     def test_product_listing(self):
@@ -20,13 +23,24 @@ class ProductTest(TestCase):
         self.assertEqual(f'{self.product.seller}','okenny john')
         self.assertEqual(f'{self.product.price}','29.99')
 
-    def test_product_list_view(self):
+    def test_product_list_view_for_login_user(self):
+        self.client.login(email='reviewuser@email.com',password='frank123')
         response = self.client.get(reverse('product_list'))
         self.assertEqual(response.status_code,200)
         self.assertContains(response,"Laptop")
         self.assertTemplateUsed(response,'products/product_list.html')
 
-    def test_product_detail_view(self):
+    def test_product_list_view_for_logout_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('product_list'))
+        self.assertEqual(response.status_code,302)
+        self.assertRedirects(response,'%s?next=/products/' % (reverse('account_login')))
+        response = self.client.get('%s?next=/products/' % (reverse('account_login')))
+        self.assertContains(response,'Log In')
+
+    def test_product_detail_view_with_permission(self):
+        self.client.login(email='reviewuser@email.com',password='frank123')
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.product.get_absolute_url())
         no_response = self.client.get('/products/12345/')
         self.assertEqual(response.status_code,200)
--- products/views.py
+++ products/views.py
@@ -1,15 +1,20 @@
 
 from django.views.generic import ListView,DetailView
 
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+
 from .models import Product
 
 
-class ProductListView(ListView):
+class ProductListView(LoginRequiredMixin,ListView):
 
     model =  Product
     template_name ='products/product_list.html'
+    login_url = 'account_login'
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, PermissionRequiredMixin,DetailView):
     model = Product
     template_name = 'products/product_detail.html'
+    login_url = 'account_login'
+    permission_required = 'products.special_status'
